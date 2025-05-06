0ea81cb5cbf2f57da3145516d443a33f34cf3d38
funderburkjim <funderburkjim@gmail.com>
2025-05-05 21:12:46 -0400

    edit keydoc/redo_one.sh :  python -> python3 for cologne #5

--- keydoc/redo_one.sh
+++ keydoc/redo_one.sh
@@ -14,23 +14,23 @@ fi
 keydoc1="$dir/keydoc1.txt"
 hwfile="distincthws/data/${dict}_hws.txt"
 multifile="distincthws/data/${dict}_multi.txt"
-python keydoc1.py $dict $hwfile $multifile $keydoc1
+python3 keydoc1.py $dict $hwfile $multifile $keydoc1
 
 # step 2
 keydoc1x="$dir/keydoc1x.txt"
 hwextra="distincthws/data/${dict}_hwextra.txt"
-python keydoc1x.py $dict $keydoc1 $hwextra $keydoc1x
+python3 keydoc1x.py $dict $keydoc1 $hwextra $keydoc1x
 
 # step 3
 keydocnorm="$dir/keydoc1x_norm.txt"
 echo "Compute $keydocnorm"
-python keydoc1x_norm.py $dict $keydoc1x $keydocnorm
+python3 keydoc1x_norm.py $dict $keydoc1x $keydocnorm
 
 # step 4
 keydocnorm1="$dir/keydoc1x_norm1.txt"
 echo "Compute $keydocnorm1"
 multidir="distinctfiles/multi"
 # use all ${multidir}/multi_*.txt files
-python keydoc1x_norm1.py $dict $keydocnorm $multidir $keydocnorm1
+python3 keydoc1x_norm1.py $dict $keydocnorm $multidir $keydocnorm1
 
 echo "END redo_one.sh for $dict"
